d4198ed6e273cb34d4be9f0edf807846d482062a
yash <yashrajsingh7890@gmail.com>
2023-07-07 17:22:10 +0530

    resolved

--- public/enroll.js
+++ public/enroll.js
@@ -1,27 +1,2 @@
 
 
-const form=document.getElementById('form');
-form
-.addEventListener('submit',function(event){
-    event.preventDefault()
-    let country=document.getElementById('input').value.trim();
-    fetch("https://restcountries.com/v3.1/name/"+country+"?fullText=true").then(function(res){
-       
-        return res.json();
-    }).then((res)=>{
-
-    if(res.status===404||res.length===0){
-        swal({
-            title: "Error!",
-            text: "Please enter a valid country",
-            type: "error",
-            confirmButtonText: "Try again",
-            confirmButtonColor:"#515151",
-            allowOutsideClick: "true"
-          });
-    }
-    else{
-        form.submit();
-    }
-    })
-});
